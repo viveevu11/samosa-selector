6e7b867751cc1d668efa0f41b6b2fa25b445198f
Vivian Vu <vivivu16@gmail.com>
2026-06-11 19:32:17 -0700

    Stage 1

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import samosa from './assets/samosa.png';
 
 const App = () => {
 
   return (
     <div className="App">
-
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: </h2>
+        <img className="samosa" src={samosa} alt="Samosa"/>
+      </div>
     </div>
   )
 }
